22551a47228c0c056dc3a9d3269f47d5746d5265
alienx5499 <p2005p5499p@gmail.com>
2026-04-04 04:34:53 +0530

    chore(ios): initial IgnisNode SwiftUI project

---
+++ IgnisNode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IgnisNode
+//
+//  Created by PRABAL PATRA on 04/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IgnisNode/IgnisNodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgnisNodeApp.swift
+//  IgnisNode
+//
+//  Created by PRABAL PATRA on 04/04/26.
+//
+
+import SwiftUI
+
+@main
+struct IgnisNodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
